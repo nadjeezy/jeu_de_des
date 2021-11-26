a6834aa7e3bd98cf26dd37ba6c025a704a87ca98
Nagi Karaouzene <nagi.karaouzene@outlook.com>
2021-11-26 17:03:10 +0100

    fix main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from partie import Partie
 
 class Main:
     if __name__ == '__main__':
-        partie = Partie(10,3)
+        partie = Partie(int(input("Veuillez entrer le nombre de tours : ")),int(input("Veuillez entrer le nombre de dés :")))
         partie.initialiser()
         partie.lancer()
         partie.afficher_gagnant()
